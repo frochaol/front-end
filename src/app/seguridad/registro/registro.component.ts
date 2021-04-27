ccd181ff7773294e1a2353582779f43c873a349e
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2021-04-26 23:01:48 -0500

    Added login process

--- src/app/seguridad/registro/registro.component.ts
+++ src/app/seguridad/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { parsearErroresAPI } from 'src/app/utilidades/utilidades';
 import { credencialesUsuario } from '../seguridad';
 import { SeguridadService } from '../seguridad.service';
@@ -10,7 +11,7 @@ import { SeguridadService } from '../seguridad.service';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor(private seguridadService: SeguridadService) { }
+  constructor(private seguridadService: SeguridadService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +20,8 @@ export class RegistroComponent implements OnInit {
   registrar(credenciales: credencialesUsuario){
     this.seguridadService.registrar(credenciales)
       .subscribe(respuesta =>{
-        console.log(respuesta);
+        this.seguridadService.guardarToken(respuesta);
+        this.router.navigate(['/']);
       }, errores => this.errores = parsearErroresAPI(errores));
   }
 
